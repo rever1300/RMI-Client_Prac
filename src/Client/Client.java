9b9d2fc834b2edada8c40765c9b43cdfeff58a8d
anasanchez28 <asanchezplumed@gmail.com>
2021-05-12 10:40:17 +0200

    modify lookup + added ExamModels

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -7,10 +7,13 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.Scanner;
 
-public class Student {
+import ExamModels.QuestionsLike;
+
+public class Client {
 
     public static void main(String[] args){
         String host = (args.length <1) ? null : args[0];
+
         try {
             Registry registry = LocateRegistry.getRegistry(host);
             StudentImpl student = new StudentImpl();
@@ -19,7 +22,19 @@ public class Student {
             Scanner scanner = new Scanner(System.in);
             String name = scanner.nextLine();
             stub.register(student, name);
-            System.out.println("Student join, waiting to start de exam");
+            System.out.println("Client join, waiting to start de exam");
+
+            synchronized (student){
+                student.wait();
+                while(true){
+                    QuestionsLike questionsLike = student.setAnswer();
+                    stub.getAnswer(name, questionsLike);
+
+                }
+            }
+
+
+
 
         }catch (Exception e){
             System.err.println("Client exception: " +e.toString()); e.printStackTrace();
